48805c56e49fa2d65a1134d56841fe9d6f4ef468
grimcoder <taraskovtun@gmail.com>
2015-09-22 12:26:07 -0700

    initial commit

---
+++ tetris/tetris/MainWindow.xaml.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace tetris
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public const int Size = 50;
+        public const int Padding = 1;
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitBoard();
+            DrawBoard();
+
+            DrawTiles();
+
+        }
+
+        private void DrawTiles()
+        {
+            
+        }
+
+        public void InitBoard()
+        {
+            Board.Width = 10;
+            Board.Height = 10;
+
+            Board.Tiles = new List<Tile>();
+             
+            Board.Tiles.AddRange(new Tile[]
+            {
+                new Tile(0,0),
+                new Tile(1,0),
+                new Tile(2,0),
+                new Tile(3,0),
+                new Tile(4,0),
+                new Tile(4,1),
+                new Tile(5,2),
+            }
+        );
+
+        }
+
+        private void DrawBoard()
+        {
+            //Grid.ShowGridLines = true;
+            
+            Grid.RowDefinitions.Clear();
+            for (int x = 0; x < Board.Width; x++)
+            {
+                Grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(Size) });
+                TilesGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(Size) });
+            }
+            
+            for (int x = 0; x < Board.Height; x++)
+            {
+                Grid.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(Size) });
+                TilesGrid.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(Size) });
+            }
+
+
+            for (int x = 0; x < Board.Width; x++)
+            {
+                for (int y = 0; y < Board.Height; y++)
+                {
+                    Border border = new Border();
+
+                    Grid.Children.Add(border);
+
+                    border.Width = Size - Padding * 2;
+                    border.Height = Size - Padding * 2;
+                    Grid.SetColumn(border, y);
+                    Grid.SetRow(border, x);
+                    border.Background = new SolidColorBrush(Colors.Beige);
+                    border.Padding = new Thickness(0);
+
+                }
+            }
+
+        }
+
+        public Board Board = new Board();
+
+
+        private void Grid_OnKeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+
+
+                case Key.Up:
+                    Debug.WriteLine("key up");
+                    break;   
+             
+                case Key.Down:
+                    Debug.WriteLine("key down");
+                    break;   
+             
+                case Key.Right:
+                    Debug.WriteLine("key right");
+                    break;     
+           
+                case Key.Left:
+                    Debug.WriteLine("key left");
+                    break;
+
+
+            }
+        }
+
+
+    }
+}
---
+++ tetris/tetris/board.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tetris
+{
+    public class Board
+    {
+        public int Height;
+        public int Width;
+        public List<Tile> Tiles = new List<Tile>();
+
+        public Figure ActiveFigure;
+    }
+
+    public class Figure
+    {
+        public void RotateLeft()
+        {}
+
+        public void RotateRight()
+        {}
+
+        public List<Tile> Tiles;
+
+    }
+
+    public class Tile
+    {
+        public Tile(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public int X;
+
+        public int Y;
+
+    }
+}
